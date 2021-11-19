5ce68c1424783b961474490300ebee49823ca56d
lnm011223 <2969827073@qq.com>
2021-11-19 10:04:21 +0800

    add remember password

--- app/src/main/java/com/lnm011223/login/MainActivity.kt
+++ app/src/main/java/com/lnm011223/login/MainActivity.kt
@@ -43,14 +43,32 @@ class MainActivity : AppCompatActivity() {
         insetsController?.hide(WindowInsetsCompat.Type.navigationBars())
         insetsController?.hide(WindowInsetsCompat.Type.statusBars())
         window.statusBarColor = Color.TRANSPARENT
-
+        var flag2 = false
+        val prefs = getPreferences(Context.MODE_PRIVATE)
+        val isremember = prefs.getBoolean("remember_password",false)
+        if (isremember) {
+            flag2 = true
+            val account_remember = prefs.getString("account","")
+            val password_remember = prefs.getString("password","")
+            signin_accountEdit.setText(account_remember)
+            signin_passwordEdit.setText(password_remember)
+            remember_check.isChecked = true
+        }
         signin_Button.setOnClickListener {
             val account = signin_accountEdit.text.toString()
             val password = signin_passwordEdit.text.toString()
-
+            val editor = prefs.edit()
             counter--
-            if (account != "" && password != "" && account == account_exist && password == password_exist){
+            if (account != "" && password != "" && account == account_exist && password == password_exist || flag2){
                 flag1 = false
+                if (remember_check.isChecked) {
+                    editor.putBoolean("remember_password",true)
+                    editor.putString("account",account)
+                    editor.putString("password",password)
+                }else{
+                    editor.clear()
+                }
+                editor.apply()
                 val intent = Intent(this,userActivity::class.java)
                 intent.putExtra("account",account)
                 startActivity(intent)
